00f4baa2e417b9c2a9b9049613076d65411c2c4a
richardbonneau <Richard.Bonneau@goto.com>
2022-06-29 19:11:24 -0400

    workinprogress: clear steps when switching instrument

--- GlobalScripts/InstrumentsManager.gd
+++ GlobalScripts/InstrumentsManager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var current_instrument
+
 var available_notes = {
 	"percussion": [
 		"kick","snare","hihat_closed","hihat_open","ride","tom1","tom2","tom3","crash1","crash2","crash3","cowbell"
@@ -22,15 +24,16 @@ var instruments = {
 }
 
 func change_instrument(new_instrument):
+	current_instrument = new_instrument
 	var notes = instruments[new_instrument].notes
 	var notes_type = instruments[new_instrument].notes_types
 	
 	for i in GlobalVariables.LABELS_ROW_LOCATION_IN_TREE.get_child_count():
-		#	Rename Labels
+		#Rename Labels
 		var label = GlobalVariables.LABELS_ROW_LOCATION_IN_TREE.get_child(i)
 		label.text = notes[i]
 		
-	#	Change the associated notes and pitch on the StepRows
+		#Change the associated notes and pitch on the StepRows
 		var step_row = GlobalVariables.NOTES_ROWS_LOCATION_IN_TREE.get_child(i)
 		step_row.note = notes[i]
 		if notes_type == "percussion":
@@ -38,7 +41,21 @@ func change_instrument(new_instrument):
 		elif notes_type == "melodic":
 			step_row.sound_stream = instruments[new_instrument].sound_stream.duplicate()
 			step_row.sound_stream.mix_rate = set_instrument_pitch(i)
-	
+		
+		# Color the steps for the new instrument
+		for j in step_row.get_child_count():
+			var step = step_row.get_child(j)
+			# If we hit a separator, skip that iteration
+			if step is ColorRect: continue
+			# Uncolor Every step
+			step.uncolor_step()
+		
+		# Color the steps for notes of that particular instrument that are played
+		var active_notes_for_this_row = SequencerPlayer.current_notes[i]
+		for active_note in active_notes_for_this_row:
+			if active_note.instrument != current_instrument: continue
+			var step = instance_from_id(active_note.step_id)
+			step.color_step()
 
 func set_instrument_pitch(note_index):
 	var pitch = 1 + (0.05 * note_index)
--- Sequencer/Step/Step.gd
+++ Sequencer/Step/Step.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var id
 export var step_number = 0
 
 var is_clicked = false
@@ -7,34 +8,40 @@ var mouse_over = false
 
 var scene_instance
 
+func _ready():
+	id = get_instance_id()
+
+func _on_ColorRect_mouse_entered():
+	mouse_over = true
+func _on_ColorRect_mouse_exited():
+	mouse_over = false
 func _on_ColorRect_gui_input(event):
 	if event is InputEventMouseButton:
 		if mouse_over and event.pressed:
 			if not is_clicked:
-				$ColorRect.color = Colors.step_picked
-				is_clicked = true
 				step_selected()
 			else:
-				$ColorRect.color = Colors.step_default
-				is_clicked = false
 				step_unselected()
 
-func _on_ColorRect_mouse_entered():
-	mouse_over = true
-
-func _on_ColorRect_mouse_exited():
-	mouse_over = false
-
 func step_selected():
+	color_step()
 #	Instantiate a Sound Scene. Reference it in node_to_add
 	scene_instance = GlobalVariables.SOUND_SCENE.instance()
 	GlobalVariables.SEQUENCER_LOCATION_IN_TREE.add_child(scene_instance)
 	var sound_stream = get_parent().sound_stream
-	
 	scene_instance.set_stream(sound_stream)
 	
-	var note_to_add = {"node_ref": scene_instance, "step_id": get_instance_id() }
+	var note_to_add = {"node_ref": scene_instance, "step_id": id, "instrument": InstrumentsManager.current_instrument }
 	SequencerPlayer.add_note(note_to_add, step_number)
 
 func step_unselected():
-	SequencerPlayer.remove_note(get_instance_id(), step_number)
+	uncolor_step()
+	SequencerPlayer.remove_note(id, step_number)
+
+func color_step():
+	$ColorRect.color = Colors.step_picked
+	is_clicked = true
+
+func uncolor_step():
+	$ColorRect.color = Colors.step_default
+	is_clicked = false
